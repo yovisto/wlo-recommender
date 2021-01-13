b0f9a28bbf723bcebef34e900c4441d5f58d04d2
joerg <joerg@yovisto.com>
2021-01-13 12:18:05 +0100

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -4,34 +4,43 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import numpy as np
 
+class Recommender:
+	doc_weights, ids = None, None
 
-modelFile = sys.argv[1]
-idFile = sys.argv[2]
-document = [sys.argv[3]]
+	def __init__(self, modelFile, idFile):
+		# LOAD MODEL AND LABELS
+		model = tf.keras.models.load_model(modelFile)
+		self.ids = np.load(idFile, allow_pickle=True)
+		doc_layer = model.get_layer('doc_embedding')
+		self.doc_weights = doc_layer.get_weights()[0]
+		self.doc_weights = self.doc_weights / np.linalg.norm(self.doc_weights, axis = 1).reshape((-1, 1))
 
-print ("Predicting: '" + document[0] + "'")
+	def run(self, doc):
+		if not doc in self.ids.values:
+			print ("Cannot find document!")
+			return []
 
-# LOAD MODEL AND LABELS
-model = tf.keras.models.load_model(modelFile)
-ids = np.load(idFile, allow_pickle=True)
+		docNum = self.ids[self.ids == doc].index[0]
 
-if not document[0] in ids.values:
-	print ("Cannot find document!")
-	sys.exit(1)
+		# PREDICTION
+		dists = np.dot(self.doc_weights, self.doc_weights[docNum])
+		sorted_ids = np.argsort(dists)
+		sorted_dists = sorted(dists)[-11:]
+		closest_ids = sorted_ids[-11:]
 
-docNum = ids[ids == document[0]].index[0]
+		result = []
+		for i in range(len(closest_ids)):
+			result.append((self.ids.values[closest_ids[i]], sorted_dists[i].astype(float)))
 
-doc_layer = model.get_layer('doc_embedding')
-doc_weights = doc_layer.get_weights()[0]
-#doc_weights.shape
+		result.reverse()
+		return result
 
-doc_weights = doc_weights / np.linalg.norm(doc_weights, axis = 1).reshape((-1, 1))
 
-# PREDICTION
-dists = np.dot(doc_weights, doc_weights[docNum])
-sorted_dists = np.argsort(dists)
-closest = reversed(sorted_dists[-10:])
+if __name__ == '__main__':
+	modelFile = sys.argv[1]
+	idFile = sys.argv[2]
+	document = sys.argv[3]
 
-for d in closest:
-    #print (d)
-    print (ids.values[d])
+	r = Recommender(modelFile, idFile)
+	for r in r.run(document):
+		print (r)
